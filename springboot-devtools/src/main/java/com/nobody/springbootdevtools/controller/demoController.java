134e55adc75d09a0a493ba1547bbd5ba35997de3
lixiao_chen <lixiao_chen@kingdee.com>
2020-08-20 12:32:00 +0800

    Initial commit

---
+++ springboot-devtools/src/main/java/com/nobody/springbootdevtools/controller/demoController.java
@@ -0,0 +1,19 @@
+package com.nobody.springbootdevtools.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author Mr.nobody
+ * @date 2020/8/20
+ */
+@RestController
+public class demoController {
+
+    @GetMapping("demo")
+    public String demo() {
+        return "Hello World!!!10100";
+    }
+
+}
